93a17d901c8b63012b45824afc39436920895ac3
Yakshdeep Kaul <ykaul3@gatech.edu>
2016-04-07 17:55:09 -0400

    added config file lookup

--- Python/Src/master.py
+++ Python/Src/master.py
@@ -15,7 +15,8 @@ from threading import Thread, Lock
 
 mutex = Lock()
 
-dbnodes_ip_hash={}
+dbnodes_ip_hash=[]
+dbnodes_ip=[]
 rcount = 2
 wcount = 2
 replicas = 3
@@ -28,6 +29,47 @@ class KeyValueInfo:
         self.key = key
         self.value = value
         self.time_stamp = time_stamp
+
+
+def lookup_ip(key_hash):
+
+    print'looking up ip'
+
+    global dbnodes_ip,dbnodes_ip_hash
+    prev = 0
+    ip_array= [None] * 3
+    for i,ip_hash in enumerate(dbnodes_ip_hash):
+
+        if(key_hash > prev and key_hash <= ip_hash):
+            ip_array[0] = dbnodes_ip[i]
+            ip_array[1] = dbnodes_ip[i+1 % 5]
+            ip_array[2] = dbnodes_ip[i+2 % 5]
+            print 'found in loop'
+            return ip_array
+
+        else:
+            prev = ip_hash
+
+    ip_array[0] = dbnodes_ip[0]
+    ip_array[1] = dbnodes_ip[1]
+    ip_array[2] = dbnodes_ip[2]
+    print 'not found in loop'
+    return ip_array
+
+
+
+
+def load_ip_hash():
+
+    global dbnodes_ip,dbnodes_ip_hash
+    
+    with open('test_config.txt') as fp:
+        for line in fp:
+            if line:
+                dbnodes_ip_hash.append(line.split(",")[0])
+                dbnodes_ip.append(line.split(",")[1])
+                
+    print dbnodes_ip
 
 
 #Function for handling connections. This will be used to create threads
@@ -46,13 +88,17 @@ def clientthread(conn):
             # hashkey = hashlib.sha256(data['KEY']).hexdigest()   #key hashing. uncomment this and the next line.
             # data['KEY'] = hashkey
             
-            data['TIMESTAMP'] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+            
             client_key=data['KEY']
             key_hash=hashlib.sha256(client_key).hexdigest()
+            # ip='24.72.242.230'
+            # print hashlib.sha256(ip).hexdigest()
             data['KEY'] = key_hash
 
             if(data['METHOD']== 'GET'): 
                 rep=get_from_dbnodes(data,0)
+                if(rep=='404'):
+                    rep='Key Not Found'
             elif(data['METHOD']=='POST'):
                 rep=post_to_dbnodes(data)
             else:
@@ -81,7 +127,7 @@ def clientthread(conn):
     conn.close()
 
 
-def post_to_dbnode3(data):
+def post_to_dbnode3(data,ip):
     s3 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         # Create a socket object
     # bind_port= 12334
     try:
@@ -90,7 +136,7 @@ def post_to_dbnode3(data):
     except socket.error as msg:
         print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
         sys.exit()
-    host1 = '24.72.242.230'      # Get local machine name
+    host1 = ip      # Get local machine name
     port1 = 12357                # Reserve a port for your service.
     
     try:
@@ -131,13 +177,21 @@ def post_to_dbnodes(data):
     getdata['METHOD'] = 'GET'
     getdata = get_from_dbnodes(getdata,1)
     print 'I finished get'
+    print getdata
 
     if(getdata=='Server Down,Please Try again in a few seconds.'):
         mutex.release()
         return 'Server Down,Please Try again in a few seconds.'
-
-    data['VECTOR_CLOCK'] = getdata['VECTOR_CLOCK']
+    elif(getdata=='404'):
+       data['VECTOR_CLOCK'] = {}
+       data['TIMESTAMP'] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+    else:
+        data['VECTOR_CLOCK'] = getdata['VECTOR_CLOCK']
+        data['TIMESTAMP'] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
     # needs to increase its own vector clock by 1
+
+    ip_array=lookup_ip(data['KEY'])
+    print ip_array
    
     s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)        # Create a socket object
     s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         # Create a socket object
@@ -159,12 +213,12 @@ def post_to_dbnodes(data):
         print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
         mutex.release()
         sys.exit()
-
-    host1 = '24.72.242.230'      # Get local machine name
+    print ip_array[0].split("\n")[0]
+    host1 = ip_array[0].split("\n")[0]      # Get local machine name
     port1 = 12338                # Reserve a port for your service.
     s1.connect((host1, port1))
 
-    host2 = '24.72.242.230'      # Get local machine name
+    host2 = ip_array[1].split("\n")[0]      # Get local machine name
     port2 = 12359                # Reserve a port for your service.
     s2.connect((host2, port2))
 
@@ -203,7 +257,7 @@ def post_to_dbnodes(data):
     elif((reply1=='404' and reply2=='OK') or (reply1=='OK' and reply2=='404') ):
         # Since one of the two primary servers failed 
         # to respond try the third server.
-        n3_rep=post_to_dbnode3(data)
+        n3_rep=post_to_dbnode3(data,ip_array[2].split("\n")[0])
         if (n3_rep=='OK'):
             s1.close()
             s2.close()
@@ -251,11 +305,13 @@ def get_from_dbnodes(data,flag):
             mutex.release()
         sys.exit()
 
-    host1 = '24.72.242.230'      # Get local machine name
+    ip_array=lookup_ip(data['KEY'])
+
+    host1 = ip_array[0].split("\n")[0]       # Get local machine name
     port1 = 12338                # Reserve a port for your service.
     
 
-    host2 = '24.72.242.230'      # Get local machine name
+    host2 = ip_array[1].split("\n")[0]       # Get local machine name
     port2 = 12359                # Reserve a port for your service.
 
     try:
@@ -289,6 +345,9 @@ def get_from_dbnodes(data,flag):
         if(flag==0):
             mutex.release()
         return recent_reply
+
+    if(reply1=='404' or reply2=='404'):
+        return '404'
 
     conflict = conflict_check(reply1['VECTOR_CLOCK'], reply2['VECTOR_CLOCK'])
     merged_vc = dict(sorted(chain(reply1['VECTOR_CLOCK'].items(), reply2['VECTOR_CLOCK'].items()), key=lambda t: t[1]))
@@ -358,14 +417,16 @@ print 'Socket bind complete'
 s.listen(10)
 print 'Socket now listening'
 
+load_ip_hash()
+
 #now keep talking with the client
 while 1:
     #wait to accept a connection - blocking call
     conn, addr = s.accept()
-    me=socket.gethostbyname(socket.gethostname())
-    server_ip_digest=hashlib.sha256(me).digest()
-    dbnodes_ip_hash[me]=server_ip_digest
-    print dbnodes_ip_hash
+    # me=socket.gethostbyname(socket.gethostname())
+    # server_ip_digest=hashlib.sha256(me).digest()
+    # dbnodes_ip_hash[me]=server_ip_digest
+    # print dbnodes_ip_hash
     print 'Connected with ' + addr[0] + ':' + str(addr[1])
      
     #start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
